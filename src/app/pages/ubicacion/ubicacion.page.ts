60ea8233e0818b97eb6167832a3c2de65b90d2a1
John Andrés <johnandrescv@gmail.com>
2020-08-06 20:41:09 -0500

    Mejoras en la pagina de ubicacion

--- src/app/pages/ubicacion/ubicacion.page.ts
+++ src/app/pages/ubicacion/ubicacion.page.ts
@@ -12,11 +12,13 @@ declare var google;
 export class UbicacionPage implements OnInit {
   @ViewChild('map',  {static: false}) mapElement: ElementRef;
   map: any;
-  ubicacion = { 
+  ubicacion = {
     direccion: '',
     lat: 0,
     lng: 0
   };
+  marker: any;
+  conf: any;
   autocompleteItems = [];
   GoogleAutocomplete: any;
 
@@ -37,73 +39,117 @@ export class UbicacionPage implements OnInit {
   }
 
   loadMap() {
-    let latLng = new google.maps.LatLng(this.ubicacion.lat, this.ubicacion.lng);
-      let mapOptions = {
-        center: latLng,
-        zoom: 15,
-        panControl: false,
-        mapTypeControl: false,
-        streetViewControl: false,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        styles: environment.googleMapsStyles,
-      } 
-      
-      this.getAddressFromCoords(this.ubicacion.lat, this.ubicacion.lng); 
-      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions); 
-      this.map.addListener('tilesloaded', () => {
-        console.log('accuracy',this.map, this.map.center.lat());
-        this.getAddressFromCoords(this.map.center.lat(), this.map.center.lng())
-        this.ubicacion.lat = this.map.center.lat()
-        this.ubicacion.lng = this.map.center.lng()
-      }); 
+    const latLng = new google.maps.LatLng(this.ubicacion.lat, this.ubicacion.lng);
+    const mapOptions = {
+      center: latLng,
+      zoom: 15,
+      panControl: false,
+      mapTypeControl: false,
+      streetViewControl: false,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      styles: environment.googleMapsStyles,
+    };
+
+    this.getAddressFromCoords(this.ubicacion.lat, this.ubicacion.lng);
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    this.marker = new google.maps.Marker({
+      position: latLng,
+      map: this.map,
+    });
+
+    this.marker.setIcon('./assets/images/marker.svg');
+
+    google.maps.event.addListener(this.map, 'click', (event: any) => {
+      const result = [event.latLng.lat(), event.latLng.lng()];
+      this.conf = {
+        contador: 0,
+        deltaLat: (result[0] - this.ubicacion.lat) / 100,
+        deltaLng: (result[1] - this.ubicacion.lng) / 100,
+        delay: 10,
+      };
+      this.moveMarker();
+    });
   }
 
-  getAddressFromCoords(lattitude, longitude) {
-    let options: NativeGeocoderOptions = {
+  getAddressFromCoords(lat, lng) {
+    const options: NativeGeocoderOptions = {
       useLocale: true,
-      maxResults: 5    
-    }; 
-    this.nativeGeocoder.reverseGeocode(lattitude, longitude, options)
-      .then((result: NativeGeocoderResult[]) => {
-        this.ubicacion.direccion = "";
-        let responseAddress = [];
-        for (let [key, value] of Object.entries(result[0])) {
-          if ( value.length > 0 )
-          responseAddress.push(value); 
+      maxResults: 5
+    };
+    this.nativeGeocoder.reverseGeocode(lat, lng, options).then((result: NativeGeocoderResult[]) => {
+        this.ubicacion.direccion = '';
+        const responseAddress = [];
+        for (const [key, value] of Object.entries(result[0])) {
+          if ( value.length > 0 ){
+            responseAddress.push(value);
+          }
         }
         responseAddress.reverse();
-        for (let value of responseAddress) {
-          this.ubicacion.direccion += value+", ";
+        for (const value of responseAddress) {
+          this.ubicacion.direccion += value + ', ';
         }
         this.ubicacion.direccion = this.ubicacion.direccion.slice(0, -2);
       })
-      .catch((error: any) =>{ 
-        this.ubicacion.direccion = "Address Not Available!";
-      }); 
+      .catch((error: any) => {
+        console.log(error);
+        this.ubicacion.direccion = 'Dirección Personalizada';
+      });
+  }
+
+  getCoordsFromAddress(dir) {
+    this.nativeGeocoder.forwardGeocode(dir).then((result) => {
+        console.log(result);
+      })
+      .catch((error: any) => {
+        console.log(error);
+      });
   }
 
   UpdateSearchResults(){
-    if (this.ubicacion.direccion == '') {
+    if (this.ubicacion.direccion === '') {
       this.autocompleteItems = [];
       return;
     }
-    this.GoogleAutocomplete.getPlacePredictions({input: this.ubicacion.direccion, componentRestrictions: {country: 'ec'}},
+    this.GoogleAutocomplete.getPlacePredictions({input: this.ubicacion.direccion, componentRestrictions: {country: 'ec'}, fields: ['geometry']},
     (predictions, status) => {
       this.autocompleteItems = [];
       this.zone.run(() => {
-        predictions.forEach((prediction) => {
-          this.autocompleteItems.push(prediction);
-        });
+        if (predictions) {
+          predictions.forEach((prediction) => {
+            this.autocompleteItems.push(prediction);
+          });
+        }
       });
     });
   }
 
   SelectSearchResult(item) {
     console.log(item);
+    this.getCoordsFromAddress(item.description);
+
+    // this.ubicacion = {
+    //   direccion: item.description,
+
+    // };
   }
 
   ClearAutocomplete(){
-    this.autocompleteItems = []
-    this.ubicacion.direccion = ''
+    this.autocompleteItems = [];
+    this.ubicacion.direccion = '';
+  }
+
+  moveMarker() {
+    this.ubicacion.lat += this.conf.deltaLat;
+    this.ubicacion.lng += this.conf.deltaLng;
+    const latlng = new google.maps.LatLng(this.ubicacion.lat, this.ubicacion.lng);
+    // marker.setTitle("Latitude:"+position[0]+" | Longitude:"+position[1]);
+    this.marker.setPosition(latlng);
+    if (this.conf.contador !== 100) {
+        this.conf.contador++;
+        setTimeout( () => this.moveMarker(), 10);
+    } else {
+      this.getAddressFromCoords(this.ubicacion.lat, this.ubicacion.lng);
+    }
   }
 }
